5d44d9eeb78f75370ffc42d32ef643c9eee0ed2e
luckyrock9 <dhruvhahaha@gmail.com>
2026-08-22 01:01:38 +0000

    adsafdsf

---
+++ cards/decks.js
@@ -0,0 +1,11 @@
+let states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR"]
+let capitals = ["Montgomery", "Juneau", "Phoenix", "Little Rock", "Sacramento", "Denver", "Hartford", "Dover", "Tallahassee", "Atlanta", "Honolulu", "Boise", "Springfield", "Indianapolis", "Des Moines", "Topeka", "Frankfort", "Baton Rouge", "Augusta", "Annapolis", "Boston", "Lansing", "St. Paul", "Jackson", "Jefferson City", "Helena", "Lincoln", "Carson City", "Concord", "Trenton", "Santa Fe", "Albany", "Raleigh", "Bismarck", "Columbus", "Oklahoma City", "Salem"];
+let set = document.getElementById("set");
+let pair = document.createElement("div");
+pair.classList.add("pair");
+let card = document.createElement("div");
+card.classList.add("card");
+pair.appendChild(card);
+pair.appendChild(card);
+set.appendChild(pair);
+document.querySelector(".card");
